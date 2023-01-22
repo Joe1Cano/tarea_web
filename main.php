19715ec99bba0798a1da0028cb99d57fbf118c75
Joe1Cano <canojoel55@hotmail.com>
2023-01-22 14:33:19 -0600

    cambios al main

--- main.php
+++ main.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ahorcado :)</title>
 </head>
-<body>
-    HOLA :)
+<body style="background: lightblue">
+    <div style="margin: 0 auto; background: #8438ff; width: 900px; height: 900px; padding: 5px; border-radius: 3px;">
+
+        <div style="display:inline-block: width: 500px; background: #000000;">
+
+            <img style="width:80%; display: inline-block;" src=""/>
+        </div>
+    </div>
     <a href="logout.php"><button>Salir</button></a>
 </body>
 </html>
